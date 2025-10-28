31e5d4fb4c41cdd5a73d0a0e95b046e89e82f281
xndadelin <adelinmihai071@gmail.com>
2025-10-28 18:21:09 +0200

    chore(encodings): add package init with explicit modules

---
+++ src/decodr/encodings/__init__.py
@@ -0,0 +1,18 @@
+"""
+decodr.encodings - collection of encoding/decoding helpers.
+
+It includes:
+- base64_mod
+- base32_mod
+- hex_mod
+- url_mod
+"""
+
+from __future__ import annotations
+
+__all__ = [
+    "base64_mod", 
+    "base32_mod",
+    "hex_mod",
+    "url_mod"
+]
