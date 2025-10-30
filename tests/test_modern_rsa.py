8bed44880941a7fc3e7e95cf285de12ac31faaf9
xndadelin <adelinmihai071@gmail.com>
2025-10-30 10:26:05 +0200

    feat(tests/rsa): add test unit for rsa

---
+++ tests/test_modern_rsa.py
@@ -0,0 +1,11 @@
+from decodr.ciphers.modern import rsa
+
+def test_rsa():
+    pub, priv = rsa._generate_keys(61, 53, 65537)
+    n_e, n_d = pub, priv
+    n, e = n_e
+    n2, d = n_d
+    assert n == n2
+    pt = "HACKCLUB"
+    ct = rsa.encrypt(pt, n, e)
+    assert rsa.decrypt(ct, n, d) == pt
