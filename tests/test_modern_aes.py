2c6a9627715a7fce24269f68b0534685e2ab19b8
xndadelin <adelinmihai071@gmail.com>
2025-10-30 10:23:16 +0200

    feat(tests/aes): add test unit for aes cipher

---
+++ tests/test_modern_aes.py
@@ -0,0 +1,14 @@
+import base64
+from decodr.ciphers.modern import aes
+
+def test_aes(monkeypatch):
+    monkeypatch.setattr(aes, "get_random_bytes", lambda n: b"\x00" * n)
+    key = "1234567890abcdef"
+    pt = "HACKCLUB"
+    ct_b64 = aes.encrypt(pt, key)
+    assert aes.decrypt(ct_b64, key) == pt
+
+    blob = base64.b64decode(ct_b64)
+    assert blob[:16] == b"\x00" * 16
+    assert (len(blob) - 16) % 16 == 0
+    
