09fdf8ba4acaa2dcb041327d2edd72bbcb44751a
xndadelin <adelinmihai071@gmail.com>
2025-10-30 12:00:26 +0200

    test(detectors): add unit test for autodetect and file magic

---
+++ tests/test_detectors.py
@@ -0,0 +1,14 @@
+from decodr.detectors import autodetect
+from decodr.detectors import file_magic
+
+def test_autodetect():
+    assert autodetect.detect_type("48656c6c6f")['type'] == 'hex'
+    assert autodetect.detect_type("SGVsbG8=")['type'] == 'base64'
+    assert autodetect.detect_type("%48%65%6C%6C%6F")['type'] == "url-encoded"
+    assert autodetect.detect_type("HELLO")["type"] in ("plaintext", "unknown")
+
+def test_file_magic(tmp_path):
+    p = tmp_path / "sample.png"
+    p.write_bytes(b"\x89PNG\r\n\x1a\n" + b"readadadakdjhakdhaskjd")
+    assert file_magic.detect_file(str(p)) == "PNG image"
+
