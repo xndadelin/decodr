9c4744e567cb07288ed89976fa9e3ff66913c3ca
xndadelin <adelinmihai071@gmail.com>
2025-10-30 10:33:19 +0200

    test(modern/hashed): verify hashes

---
+++ tests/test_modern_hashes.py
@@ -0,0 +1,16 @@
+import hashlib
+from decodr.ciphers.modern import hashes
+
+def test_hashes():
+    s = "HACKCLUB"
+    algos = [
+        "md5",
+        "sha1",
+        "sha256",
+        "sha512"
+    ]
+    for algo in algos:
+        expected = getattr(hashlib, algo)(s.encode()).hexdigest()
+        got = hashes.hash_text(s, algo)
+        assert got.lower() == expected, f"{algo} mismatch"
+        assert hashes.verify_hash(s, expected, algo), f"{algo} verify failed"
