c84799ae47b4083bce71204bcf8145f8886c3d15
xndadelin <adelinmihai071@gmail.com>
2025-11-04 20:40:52 +0200

    test(ciphers/classical): add bacon cipher text

---
+++ tests/classical/test_bacon.py
@@ -0,0 +1,9 @@
+from pydecodr.ciphers.classical import bacon
+
+def test_bacon():
+    pt = "HACKCLUB YIHA"
+    ct = bacon.encrypt(ct)
+    rt = bacon.decrypt(ct)
+
+    expected = "".join(c.upper() for c in pt if c.isalpha() or not c.isspace())
+    assert rt == expected
