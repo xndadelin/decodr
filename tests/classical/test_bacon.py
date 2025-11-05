3ef5e170e4ac493c608decae53784b3095d775e6
xndadelin <adelinmihai071@gmail.com>
2025-11-05 18:28:52 +0200

    Fix bacon test.

--- tests/classical/test_bacon.py
+++ tests/classical/test_bacon.py
@@ -2,7 +2,7 @@ from pydecodr.ciphers.classical import bacon
 
 def test_bacon():
     pt = "HACKCLUB YIHA"
-    ct = bacon.encrypt(ct)
+    ct = bacon.encrypt(pt)
     rt = bacon.decrypt(ct)
 
     expected = "".join(c.upper() for c in pt if c.isalpha() or not c.isspace())
