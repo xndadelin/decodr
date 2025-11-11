d082fe9a061a2d0a8224648aedb57e38b81e9f01
xndadelin <adelinmihai071@gmail.com>
2025-11-11 07:46:17 +0200

    Create main blueprints & start routes.

--- web/flaskapp/app/__init__.py
+++ web/flaskapp/app/__init__.py
@@ -1,19 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_cors import CORS
-import os
 
 def create_flask_app():
     app = Flask(__name__, template_folder="templates", static_folder="static")
     app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "dev")
 
-    CORS(app, resources={
-        r"/*": {
-            "origins": "*"
-        }
-    })
+    CORS(app)
 
-    @app.route("/")
-    def home():
-        return render_template("index.html")
+    from app.routes import main, api
+    app.register_blueprint(main.bp)
+    app.register_blueprint(api.bp)
     
     return app
